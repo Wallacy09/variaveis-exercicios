e5af26288607927d0e3fa1120ac81ecd19a8c8ae
Wallacy <wallacy_vinicius@hotmail.com>
2023-05-03 21:09:52 -0300

    Exercicos de javaScript

---
+++ exercicio2/script.js
@@ -0,0 +1,13 @@
+
+let pergunta = "pergunta"
+let resposta = prompt ("Você é maior de idade ?")
+    
+    let pergunta2 = "pergunta2"
+    let resposta2 = prompt  ("Você tem carro ?")
+        
+        let pergunta3 = "pergunta3"
+        let resposta3 = prompt ("Você gosta de dirigir ?")
+    
+    console.log(pergunta,resposta) 
+        console.log(pergunta2,resposta2)
+            console.log(pergunta3,resposta3)
